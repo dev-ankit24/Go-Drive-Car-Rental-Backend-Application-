86cac60737186a5b1ac57a4e0cc61c921ac48b9d
dev-ankit24 <ankitkumar839282@gmail.com>
2024-08-09 16:46:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const hbs = require("hbs")
+const app = express()
+
+app.set("view engine","hbs")
+app.use(express.static("./views/public"))
+
+hbs.registerPartials("./views/partial")
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+
+app.listen(8000, console.log("server is run http://localhost:8000"))
