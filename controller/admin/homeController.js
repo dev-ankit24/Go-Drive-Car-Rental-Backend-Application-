cf280da7e511a273dccf426a2e33cabcd57c54ee
dev-ankit24 <ankitkumar839282@gmail.com>
2024-08-24 20:12:55 +0530

    create login form

--- controller/admin/homeController.js
+++ controller/admin/homeController.js
@@ -7,7 +7,26 @@ function home(req,res){
     res.render("admin/home/login",{title:"Admin Login "})
  }
 
- function loginStore(req,res){
-    res.render("admin/home/login",{title:"Admin Login "})
+ async function loginStore(req,res){
+   try {
+      let data= await User.find({$and:[
+         {$or:[
+            {username:req.body.username},
+            {email:req.body.email}
+         ]},
+         {password:req.body.password}
+      ]})
+      if(data){
+
+      }
+   } catch (error) {
+      console.log(error);
+      
+      res.render("admin/home/login",{
+         title:"Admin Login",
+         data:data,
+
+      })
+   }
  }
 module.exports={home,login,loginStore}
