3a2c5048b207414e07f47b5a586eb312d252e199
dev-ankit24 <ankitkumar839282@gmail.com>
2024-08-30 21:36:21 +0530

    crete pic store function

--- controller/admin/homeController.js
+++ controller/admin/homeController.js
@@ -1,3 +1,4 @@
+const session = require("express-session")
 const User =require("../../models/User")
 
 async function home(req,res){
@@ -50,11 +51,50 @@ async function loginStore(req,res){
 
 //  Logout controller
 
-function logout(req,res){
-   req.session.destory("login")
-   req.session.destory("name")
-   req.session.destory("userid")
-   res.redirect("/admin/login")
 
+function logout (req, res)  {
+   req.session.destroy(err => {
+     if (err) {
+       console.error('Error destroying session:', err);
+     } else {
+       console.log('Session destroyed successfully.');
+     }
+     res.redirect('/admin/login');
+   });
+ }
+
+// profile update (image store)
+
+function profileUpdate(req,res){
+   res.render("admin/home/login",{session:req.session, title:"Admin Login"})
 }
+
+async function profileUpdateStore(req,res){
+   try {
+      let data= await User.findOne({
+         $and:[
+            {
+               $or:[
+                  {username:req.body.username},
+                  {email:req.body.emil}
+               ]
+            },
+            {password:req.body.password}
+         ]
+      })
+      if(data){
+         req.session.login=true
+         req.session.name=data.name
+         req.session.userid=data.id
+         res.redirect("/admin")
+      }
+      else{
+         res.render("admin/home/login")
+      }
+   } catch (error) {
+      console.log(error);
+      
+   }
+}
+
 module.exports={home,login,loginStore,logout}
---
+++ middelware/multerMiddleware.js
@@ -0,0 +1,21 @@
+const multer = require("multer")
+
+function makeUploader(folderName){
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, `/uploaders${folderName}`)
+        },
+        filename: function (req, file, cb) {
+          cb(null,Date.now()+file.originalname)
+        }
+      })
+      
+      const upload = multer({ storage: storage })
+      return upload
+}
+ 
+module.exports={
+    userUploader:makeUploader("users"),
+    carsUploader:makeUploader("cars"),
+testimonialUploader:makeUploader("testimonial"),
+}
