371173ce07835c8f2b46b035a7c3d34850eeb74f
dev-ankit24 <ankitkumar839282@gmail.com>
2024-09-08 16:38:38 +0530

    fix bug Contact Update or Delete Section

--- controller/admin/ContactController.js
+++ controller/admin/ContactController.js
@@ -32,7 +32,9 @@ async function remove(req,res){
 async function edit(req, res){
     try {
         let data = await Contact.findOne({_id:req.params._id})
-        res.render("admin/contact/edit",{title:" Admin User Edit Section ", data:data , error:{}})
+        data.active =false
+        await data.save()
+        res.render("admin/contact/show",{title:" Query Section ", data:data  })
     } catch (error) {
         console.log(error);
         res.redirect("/admin/contact")
